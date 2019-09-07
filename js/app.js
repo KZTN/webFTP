781aa8b12cf0d16bd72b6bfd0f4d732e5fc1c8fb
KZTN <KZTNdev@gmail.com>
2019-09-07 17:33:29 -0300

    removed console log flags

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 var btnFile = document.querySelector('#app input[name="btnFile"]');
 var btnSubmit = document.querySelector('#app input[name="btnSubmit"]');
-console.log("i'm here 1");
 
 btnFile.onchange = function enabeBtnSubmit() {
     btnSubmit.removeAttribute('disabled');
